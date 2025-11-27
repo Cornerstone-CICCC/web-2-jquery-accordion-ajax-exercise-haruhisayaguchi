d27e0863700be47b111474e68de5e5c0bc42ef97
haruhisayaguchi <hal.yaguchi@gmail.com>
2025-11-26 22:19:16 -0800

    WAD202_W2D3 completed exercise

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-$(function() {
-  // your code here
+$(function () {
+  // Exercise 1
+  $(".accordion-header").on("click", function () {
+    $(".accordion-content").slideUp();
+    if ($(this).next().is(":hidden")) {
+      $(this).next().slideDown();
+    } else {
+      $(this).next().slideUp();
+    }
+  })
+
+  // Exercise 2
+  $(".todos").first().on("click", function () {
+    $.ajax({
+      url: "https://dummyjson.com/todos",
+      method: "GET",
+      success: function (data) {
+        data.todos.forEach(item => {
+          $(".todos").find("ul").append("<li>" + item.todo + "</li>")
+        })
+      }
+    })
+  })
 })
